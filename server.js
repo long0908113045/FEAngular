d231324e001d532b137b9bbbcb634c38b2e8e2f0
Ho Phuoc Viet Long <long0908113045@gmail.com>
2022-01-16 01:00:48 +0700

    update

--- server.js
+++ server.js
@@ -3,13 +3,26 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+  // Instruct the app
+  // to use the forceSSL
+  // middleware
+  app.use(forceSSL());
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular/index.html'));
+res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
